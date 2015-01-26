6129770d3cdee5f0439057fb5c1b6941c5c4734c
hejuso <hector151293@gmail.com>
2015-01-26 09:33:36 +0100

    Ejercicio 6
    Signed-off-by: hejuso <hector151293@gmail.com>

---
+++ Ejercicios/src/TEMA5/Ejercicio6.java
@@ -0,0 +1,45 @@
+package TEMA5;
+
+public class Ejercicio6 {
+
+	// Atributos
+
+	public int x;
+	public int y;
+
+	// Constructor
+
+	public Ejercicio6(int a, int b) {
+
+		x = a;
+		y = b;
+
+	}
+
+	// Métodos
+
+	public void suma() {
+
+		System.out.println("Suma de A + B = " + (x + y));
+
+	}
+
+	public void resta() {
+
+		System.out.println("Resta de A - B = " + (x - y));
+
+	}
+
+	public void multiplicacion() {
+
+		System.out.println("Multiplicación de A * B = " + (x * y));
+
+	}
+
+	public void division() {
+
+		System.out.println("División de A / B = " + (x / y));
+
+	}
+
+}
---
+++ Ejercicios/src/TEMA5/PruebaCalculadoraEj6.java
@@ -0,0 +1,17 @@
+package TEMA5;
+
+public class PruebaCalculadoraEj6 {
+
+	public static void main(String[] args) {
+		
+		Ejercicio6 e;
+		e= new Ejercicio6(9, 6);
+		
+		e.suma();
+		e.resta();
+		e.multiplicacion();
+		e.division();
+		
+	}
+
+}
