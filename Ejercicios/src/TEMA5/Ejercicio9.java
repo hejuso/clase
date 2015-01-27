1500a7b12437394447c1c50413ba5767992afdd5
hejuso <hector151293@gmail.com>
2015-01-27 17:40:22 +0100

    Ejercicio 9. He avanzado mínimamente.
    Signed-off-by: hejuso <hector151293@gmail.com>

--- Ejercicios/src/TEMA5/Ejercicio9.java
+++ Ejercicios/src/TEMA5/Ejercicio9.java
@@ -3,9 +3,10 @@ package TEMA5;
 public class Ejercicio9 {
 
 	// Atributos
-	double dni;
+	int dni;
 	char letra;
 	char calcLetra = ' ';
+	String dniString;
 
 	// Constructores
 
@@ -16,7 +17,7 @@ public class Ejercicio9 {
 
 	}
 
-	public Ejercicio9(double adni, char aletra) {
+	public Ejercicio9(int adni, char aletra) {
 
 		dni = adni;
 		letra = aletra;
@@ -27,63 +28,92 @@ public class Ejercicio9 {
 
 	public void mostrar() {
 
-		System.out.println("Su DNI es: " + dni + "-" + letra);
+		System.out.println("Su DNI es: " + dni + "-" + calcLetra);
 
 	}
+	
+	public String acceso(){
+		
+		
+		
+		return dniString;
+	}
 
-	private char letra() {
+	public char letra() {
 
-		int resto = (int) (dni % 23);
-
+		int resto =  (dni % 23);
+		
 		switch (resto) {
-
+		
 		case 0:
 			calcLetra = 'T';
-
+			break;
 		case 1:
 			calcLetra = 'R';
+			break;
 		case 2:
 			calcLetra = 'W';
+			break;
 		case 3:
 			calcLetra = 'A';
+			break;
 		case 4:
 			calcLetra = 'G';
+			break;
 		case 5:
 			calcLetra = 'M';
+			break;
 		case 6:
 			calcLetra = 'Y';
+			break;
 		case 7:
 			calcLetra = 'F';
+			break;
 		case 8:
 			calcLetra = 'P';
+			break;
 		case 9:
 			calcLetra = 'D';
+			break;
 		case 10:
 			calcLetra = 'X';
+			break;
 		case 11:
 			calcLetra = 'B';
+			break;
 		case 12:
 			calcLetra = 'N';
+			break;
 		case 13:
 			calcLetra = 'J';
+			break;
 		case 14:
 			calcLetra = 'Z';
+			break;
 		case 15:
 			calcLetra = 'S';
+			break;
 		case 16:
 			calcLetra = 'Q';
+			break;
 		case 17:
 			calcLetra = 'V';
+			break;
 		case 18:
 			calcLetra = 'H';
+			break;
 		case 19:
 			calcLetra = 'L';
+			break;
 		case 20:
 			calcLetra = 'C';
+			break;
 		case 21:
 			calcLetra = 'K';
+			break;
 		case 22:
 			calcLetra = 'E';
+			break;
 		}
 
 		return calcLetra;
---
+++ Ejercicios/src/TEMA5/PruebaDNIEj9.java
@@ -0,0 +1,15 @@
+package TEMA5;
+
+public class PruebaDNIEj9 {
+
+	public static void main(String[] args) {
+
+		Ejercicio9 e1 = new Ejercicio9();
+		Ejercicio9 e2 = new Ejercicio9(22364548, 'R');
+
+		e2.letra();
+		e2.mostrar();
+		
+	}
+
+}
